172bfc5626ecfba6f99b5a4fd0685d6ec390547e
udachin077 <pypi.udachin@yandex.ru>
2024-12-16 19:32:34 +0300

    add loggers

---
+++ pydentity/loggers.py
@@ -0,0 +1,6 @@
+import logging
+
+user_manager_logger = logging.getLogger("pydentity.user_manager")
+role_manager_logger = logging.getLogger("pydentity.role_manager")
+sign_in_manager_logger = logging.getLogger("pydentity.signin_manager")
+data_protector_token_provider_logger = logging.getLogger("pydentity.data_protector_token_provider")
