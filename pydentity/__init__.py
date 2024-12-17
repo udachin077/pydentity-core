6923053319110fda2585b19394fc5a16e68f6707
udachin077 <pypi.udachin@yandex.ru>
2024-12-17 17:45:23 +0300

    add init

---
+++ pydentity/__init__.py
@@ -0,0 +1,16 @@
+from pydentity.dataprotector import DefaultPersonalDataProtector as DefaultPersonalDataProtector
+from pydentity.identity_constants import IdentityConstants as IdentityConstants
+from pydentity.identity_error import IdentityError as IdentityError
+from pydentity.identity_error_describer import IdentityErrorDescriber as IdentityErrorDescriber
+from pydentity.identity_options import IdentityOptions as IdentityOptions
+from pydentity.identity_result import IdentityResult as IdentityResult
+from pydentity.lookup_normalizer import (
+    UpperLookupNormalizer as UpperLookupNormalizer,
+    LowerLookupNormalizer as LowerLookupNormalizer,
+)
+from pydentity.role_manager import RoleManager as RoleManager
+from pydentity.signin_manager import SignInManager as SignInManager, SignInResult as SignInResult
+from pydentity.user_claims_principal_factory import UserClaimsPrincipalFactory as UserClaimsPrincipalFactory
+from pydentity.user_confirmation import DefaultUserConfirmation as DefaultUserConfirmation
+from pydentity.user_login_info import UserLoginInfo as UserLoginInfo
+from pydentity.user_manager import UserManager as UserManager
