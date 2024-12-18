1bd2b49c7306473296ac96e9f0109d11721834b1
udachin077 <pypi.udachin@yandex.ru>
2024-12-18 22:58:29 +0300

    update tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,6 @@
-from pydentity.utils import is_none_or_space
+import pytest
+
+from pydentity.utils import is_none_or_space, ensure_str, ensure_bytes
 
 
 def test_is_none_or_space() -> None:
@@ -9,3 +11,12 @@ def test_is_none_or_space() -> None:
     assert is_none_or_space("None") is False
     assert is_none_or_space("_") is False
     assert is_none_or_space("  .") is False
+
+@pytest.mark.parametrize("v", ["string", b"string", "string".encode()])
+def test_ensure_str(v):
+    assert isinstance(ensure_str(v), str)
+
+
+@pytest.mark.parametrize("v", ["string", b"string", "string".encode()])
+def test_ensure_bytes(v):
+    assert isinstance(ensure_bytes(v), bytes)
